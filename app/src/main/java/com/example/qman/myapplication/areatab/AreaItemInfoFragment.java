2346032e2093c4d1645364c7a216faea78221c7e
lyf-wxy <513315356@qq.com>
2017-02-20 08:44:23 +0800

    add 点击跳转产品展示界面

--- app/src/main/java/com/example/qman/myapplication/areatab/AreaItemInfoFragment.java
+++ app/src/main/java/com/example/qman/myapplication/areatab/AreaItemInfoFragment.java
@@ -5,24 +5,32 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.qman.myapplication.R;
+import com.example.qman.myapplication.utils.GalleryAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -35,12 +43,46 @@ public class AreaItemInfoFragment extends Fragment implements OnClickListener
     private String codeidStr = "";
     private Bundle savedState;//临时数据保存
 
+    private ImageView legendPic;
+
+    private RecyclerView mRecyclerView;
+    private GalleryAdapter mAdapter;
+    private List<Integer> mDatas;
+    private List<String> mdatetime;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.area_item_info_fragment, container, false);
         Intent intent= getActivity().getIntent();
+
+        initDatas();
+
+        legendPic = (ImageView) view.findViewById(R.id.legendPic);
+        //得到控件
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.id_recyclerview_horizontal);
+        //设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        //设置适配器
+        mAdapter = new GalleryAdapter(getActivity().getApplicationContext(), mDatas,mdatetime);
+
+        mAdapter.setOnItemClickListener(new GalleryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int posiition) {
+
+                //view.setEnabled(true);
+                mAdapter.setSelectIndex(posiition);
+
+                Toast.makeText(getActivity().getApplicationContext(),posiition+"",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        legendPic.setImageResource(R.drawable.legend);
 
         return view ;
     }
@@ -106,6 +148,12 @@ public class AreaItemInfoFragment extends Fragment implements OnClickListener
         Log.v("saveState()",codeidStr);
         return state;
     }
-
+    private void initDatas()
+    {
+        mDatas = new ArrayList<Integer>(Arrays.asList(R.drawable.a,
+                R.drawable.b, R.drawable.a, R.drawable.b, R.drawable.a,
+                R.drawable.b, R.drawable.a, R.drawable.b, R.drawable.a));
+        mdatetime = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8","9"));
+    }
 
 }
--- app/src/main/java/com/example/qman/myapplication/indextab/MapFragment.java
+++ app/src/main/java/com/example/qman/myapplication/indextab/MapFragment.java
@@ -6,15 +6,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISImageServiceLayer;
+import com.esri.core.map.MosaicRule;
+import com.esri.core.map.RasterFunction;
 import com.example.qman.myapplication.R;
 
 public class MapFragment extends Fragment
 {
+    private MapView mMapView;
+    private ArcGISImageServiceLayer mArcGISImageServiceLayer;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.map_fragment, container, false);
+        View view = inflater.inflate(R.layout.map_fragment,container,false);
+        mMapView = (MapView)view.findViewById(R.id.map);
+
+        mArcGISImageServiceLayer = new ArcGISImageServiceLayer("http://10.2.3.222:6080/arcgis/rest/services/sde_DBO_shangxi/ImageServer",null);
+
+        MosaicRule mMosaicRule = new MosaicRule();
+        mMosaicRule.setWhere("OBJECTID<14");
+        mArcGISImageServiceLayer.setMosaicRule(mMosaicRule);
+
+        RasterFunction renderingRule = new RasterFunction();
+        String rasterFunctionTemplate = "14";
+        renderingRule.setFunctionName(rasterFunctionTemplate);
+        mArcGISImageServiceLayer.setRenderingRule(renderingRule);
+
+        mMapView.addLayer(mArcGISImageServiceLayer);
+
+        return view;
     }
 
 }
