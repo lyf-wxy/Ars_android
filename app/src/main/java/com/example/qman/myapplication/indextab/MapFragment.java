9c96e319dd46ebc41652a50b75e49eac7d38165a
lyf-wxy <513315356@qq.com>
2017-02-20 20:45:40 +0800

    update mapfragment

--- app/src/main/java/com/example/qman/myapplication/indextab/MapFragment.java
+++ app/src/main/java/com/example/qman/myapplication/indextab/MapFragment.java
@@ -2,9 +2,11 @@ package com.example.qman.myapplication.indextab;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.esri.android.map.MapView;
 import com.esri.android.map.ags.ArcGISImageServiceLayer;
@@ -15,6 +17,9 @@ import com.example.qman.myapplication.R;
 public class MapFragment extends Fragment
 {
     private MapView mMapView;
+    FloatingActionButton mSearch;
+    FloatingActionButton mPosition;
+    FloatingActionButton mShowall;
     private ArcGISImageServiceLayer mArcGISImageServiceLayer;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +27,36 @@ public class MapFragment extends Fragment
     {
         View view = inflater.inflate(R.layout.map_fragment,container,false);
         mMapView = (MapView)view.findViewById(R.id.map);
+        mPosition = (FloatingActionButton)view.findViewById(R.id.position);
+        mSearch = (FloatingActionButton)view.findViewById(R.id.search);
+        mShowall = (FloatingActionButton)view.findViewById(R.id.showAll);
+
+        mSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+                Toast.makeText(getActivity().getApplicationContext(),"mSearch",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mPosition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+                Toast.makeText(getActivity().getApplicationContext(),"position",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mShowall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+                Toast.makeText(getActivity().getApplicationContext(),"mShowall",Toast.LENGTH_LONG).show();
+            }
+        });
 
         mArcGISImageServiceLayer = new ArcGISImageServiceLayer("http://10.2.3.222:6080/arcgis/rest/services/sde_DBO_shangxi/ImageServer",null);
 
